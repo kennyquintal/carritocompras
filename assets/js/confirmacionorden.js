ee0f10ec120112a71b8f60542348f1208b8fb1d9
Kenneth Quintal <quintalkenny@gmail.com>
2022-10-31 13:46:01 -0600

    Estados y ciudades
    Se da la funcion a los estados y ciudades

--- assets/js/confirmacionorden.js
+++ assets/js/confirmacionorden.js
@@ -1,17 +1,18 @@
+var json = {};
 let body = document.querySelector('body');
 body.onload = function() {
    printStates();
-   //printCity();
 }
 
 function printStates(){
    fetch('../assets/json/estados-municipios.json')
    .then(function(resp){return resp.json()})
    .then(function(data){
+      json = data;
       var estados = Object.getOwnPropertyNames(data);
       var selectIdStates = document.getElementById("inputState");
       var emptyOption = document.createElement("option")
-      emptyOption.textContent = "Seleccione una opción";
+      emptyOption.textContent = "Seleccione un Estado";
       emptyOption.value = "";
       selectIdStates.appendChild(emptyOption);
       for(var i = 0; i < estados.length; i++) {
@@ -22,6 +23,31 @@ function printStates(){
          selectIdStates.appendChild(el);
       }
    });   
+}
+
+let selectIdStates = document.getElementById("inputState");
+
+selectIdStates.onchange = function(){
+   var ciudad = json[selectIdStates.value];
+   console.log(selectIdStates.value);
+   cleanCity()
+   printCity(ciudad);
+}
+
+function printCity(ciudad){
+      let selectIdCity = document.getElementById("inputCity");
+      for(var i = 0; i < ciudad.length; i++) {
+         var opt = ciudad[i];
+         var el = document.createElement("option");
+         el.textContent = opt;
+         el.value = opt;
+         selectIdCity.appendChild(el);
+      }  
+}
+
+function cleanCity(){
+   let selectIdCity = document.getElementById("inputCity");
+   selectIdCity.innerHTML = "";
 }
 
 
