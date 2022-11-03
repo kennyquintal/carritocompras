c2663965fe9062a70f521cc7c9fc7fe284c03fe6
Miguel Jeronimo <mjeronimo@frd.org.mx>
2022-11-03 11:44:30 -0600

    Extración de la información de cada una de las tarjetas pulsando un boton, utilizando jquery

--- assets/js/aumento.js
+++ assets/js/aumento.js
@@ -1,106 +1,72 @@
-var inicio = 0; //se inicializa una variable en 0
+$(document).ready(function (e) {
+     productos()
+     $(document).on('click','.agregar',(e)=>{
+          //accedemos al padre de todos los elementos con jquery
+          let padre = $(this)[0].activeElement.parentElement
+          let modeloProducto = padre.children[0].innerHTML
+          let descripcionProducto = padre.children[2].children[0].innerHTML
+          let precioProducto = padre.children[2].children[1].innerHTML
+          let cantidadProducto = padre.children[4].value
+          console.log(cantidadProducto)
+     })
+})
 
-function aumentar() {
-     // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
-
-     var cantidad = document.getElementById('cantidad').value = ++inicio;
-     //se obtiene el valor del input, y se incrementa en 1 el valor que tenga.
-}
-
-function disminuir() {
-
-     var cantidad = document.getElementById('cantidad');
-
-     if (inicio < 0) {
-          alert("No se puede tener stock negativa");
-          cantidad.value = 0;
-     }
-     else {
-          cantidad.value = inicio--;
-     }
-}
-
-
-
-
-
-//Segunda tarjeta
-var principal = 0; //se inicializa una variable en 0
-
-function agregar() {
-     // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
-
-     var pieza = document.getElementById('pieza').value = ++principal;
-     //se obtiene el valor del input, y se incrementa en 1 el valor que tenga.
-}
-
-
-function quitar() {
-
-     var pieza = document.getElementById('pieza');
-
-     if (principal < 0) {
-          alert("No se puede tener stock negativa");
-          pieza.value = 0;
-     }
-     else {
-          pieza.value = principal--;
-     }
-}
-
-
-
-
-//Tercera tarjeta
-var inicial = 0; //se inicializa una variable en 0
-
-function aumenta() {
-     // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
-
-     var cantidad = document.getElementById('producto').value = ++inicial;
-     //se obtiene el valor del input, y se incrementa en 1 el valor que tenga.
-}
-
-
-function baja() {
-
-     var producto = document.getElementById('producto');
-
-     if (inicial < 0) {
-          alert("No se puede tener stock negativa");
-          producto.value = 0;
-     }
-     else {
-          producto.value = inicial--;
-     }
+function productos() {
+     let plantilla =""
+     //let contenedorProductos = document.getElementById('contenedor-productos')
+     let url = 'http://10.60.63.84:4000/api/Products/'
+     fetch(url)
+     .then(function(respuesta) {return respuesta.json()})
+     .then(function (data) {
+          //console.log(typeof(data))
+          /**
+           * extraemos la data y vamos aiterarla para sacar cada uno de los datos
+           */
+          data.forEach(datos => {
+              // console.log(datos)
+               plantilla +=`
+               <div class="col-2 tarjetas-margen">
+               <div class="card" style="width: 18rem;">
+                   <img src="/assets/img/tenis1.png" class="card-img-top" alt="...">
+                   <div class="card-body">
+                       <h5 class="card-title">${datos.Modelo}</h5>
+                       <p class="card-text">
+                       <ul>
+                           <dl>${datos.Descripcion}</dl>
+                           <dl>${datos.Precio}</dl>
+                           <span class="text-success">${datos.Cantidad} pza</span>
+                       </ul>
+                       </p>
+                       <input type='number' class="cantidad" min="0">
+                       <select class="form-select general" aria-label="Default select example">
+                           <option selected>Tallas</option>
+                           <option value="1">25</option>
+                           <option value="2">26</option>
+                           <option value="3">27</option>
+                           <option value="4">28</option>
+                           <option value="5">29</option>
+                       </select>
+                       <button class="agregar btn btn-primary general">Agregar</button>
+                   </div>
+               </div>
+           </div>
+               `
+          });
+          //
+          $("#contenedor-productos").html(plantilla)
+     })
 }
 
 
 
 
-//Cuarta tarjeta
 
-var principio = 0; //se inicializa una variable en 0
 
-function subir() {
-     // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
 
-     var articulo = document.getElementById('articulo').value = ++principio;
-     //se obtiene el valor del input, y se incrementa en 1 el valor que tenga.
+
+function obtenerInformaciónTarjetas() {
+     
 }
-function bajar() {
-
-     var articulo = document.getElementById('articulo');
-
-     if (principio < 0) {
-          alert("No se puede tener stock negativa");
-          articulo.value = 0;
-     }
-     else {
-          articulo.value = principio--;
-     }
-}
-
 
 
 
