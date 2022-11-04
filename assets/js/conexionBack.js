4ddd1cc7423545d5e90b6cd863e96403c6d0f3fc
Kenneth Quintal <quintalkenny@gmail.com>
2022-11-04 13:01:31 -0600

    se crea consulta

--- assets/js/conexionBack.js
+++ assets/js/conexionBack.js
@@ -1,9 +1,10 @@
 //consultaProductos();
 //consultaUsuarios();
 //consultaCarrito();
+insertarClientes();
 
 function consultaProductos(){
-    fetch('http://10.60.63.90:4000/api/Products/')
+    fetch('http://10.60.63.90:4000/api/registro/')
     .then(response => response.json()) //convierte a json
     .then(json => console.log(json))
  }
@@ -18,4 +19,24 @@ function consultaCarrito(){
    fetch('http://10.60.63.90:4000/api/carrito/1')
    .then(response => response.json()) //convierte a json
    .then(json => console.log(json))
+}
+
+var enviar = document.getElementById('clienteForm');
+enviar.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var data = new FormData(enviar);
+    insertarClientes(data);
+});
+
+async function insertarClientes(data) {
+   let response = await fetch('http://10.60.63.90:4000/api/registro/', {
+      method: 'POST',
+      body: data,
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+    });
+    
+    let result = await response.json();
+    console.log(result.message);
 }
